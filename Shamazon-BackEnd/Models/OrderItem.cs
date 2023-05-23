96cee7829a555928584d0299f53a74f2d2bf8830
Ethan Sawicki <ethansawicki2797@gmail.com>
2023-05-22 21:20:11 -0500

    working on orders back end

---
+++ Shamazon-BackEnd/Models/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Shamazon.Models
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public List<Products> Products { get; set; }
+
+        public int ProductQuanity { get; set; }
+    }
+}
---
+++ Shamazon-BackEnd/Repositories/OrderItemRepository.cs
@@ -0,0 +1,50 @@
+﻿using Shamazon.DBUtils;
+using Shamazon.Models;
+
+namespace Shamazon.Repositories
+{
+    public class OrderItemRepository : BaseRepository
+    {
+        public OrderItemRepository(IConfiguration configuration) : base(configuration) { }
+
+        public List<OrderItem> GetOrderItemsById(int userId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            OrderItem.id, OrderItem.OrderId, OrderItem.ProductId, OrderItem.ProductQuantity
+                        FROM OrderItem as OrderItem
+                        JOIN Orders as Orders ON Orders.id = OrderItem.OrderId
+                        JOIN Products as Products ON Products.id = OrderItem.ProductId
+                        WHERE Orders.userId = @userId";
+
+                    DbUtils.AddParameter(cmd, "@userId", userId);
+
+                    var reader = cmd.ExecuteReader();
+
+                    var orderItems = new List<OrderItem>();
+
+                    while (reader.Read())
+                    {
+                        orderItems.Add(new OrderItem()
+                        {
+                            OrderItemId = DbUtils.GetInt(reader, "id"),
+                            OrderId = DbUtils.GetInt(reader, "OrderId"),
+                            ProductQuanity = DbUtils.GetInt(reader, "ProductQuantity"),
+                            Products = new List<Products>()
+                        });
+
+                    }
+
+                    reader.Close();
+
+                    return orderItems;
+                }
+            }
+        }
+    }
+}
